fd22529d7fd2efa6eaf25d767527307d198dec67
endofdata <endofdata@hotmail.de>
2019-09-22 18:07:32 +0200

    'Hello world' discord bot based on discord.py

---
+++ betabot-201909/betabot.py
@@ -0,0 +1,30 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+key = 'BETABOT_TOKEN'
+
+if key in os.environ:
+    token = os.environ[key]
+else:
+    token = None
+
+if token == None or token == '':
+    print(f"Missing token environment variable '{key}'")
+else:
+    client.run()
+
+
