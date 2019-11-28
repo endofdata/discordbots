283999b9ef8e829fadb89fc697ef59608706174d
endofdata <endofdata@hotmail.de>
2019-11-28 19:17:40 +0100

    Attempt to run a timeout-based client.close(). Set timeout in minutes as env BETABOT_TIMEOUT.

--- betabot.py
+++ betabot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import threading
 
 client = discord.Client()
 
@@ -15,17 +16,37 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-print('Betabot is starting up.')
-key = 'BETABOT_TOKEN'
 
-if key in os.environ:
-    token = os.environ[key]
+def on_shutdown():
+    print('Betabot is closing client connection.')
+    client.close()
+
+def get_env(key):
+    if key in os.environ:
+        val = os.environ[key]
+    else:
+        val = None
+
+    return val
+
+print('Betabot is starting up.')
+env_token = 'BETABOT_TOKEN'
+env_timeout = 'BETABOT_TIMEOUT'
+
+token = get_env(env_token)
+timeout = get_env(env_timeout)
+
+if timeout == None or timeout == '':
+    print(f"No timeout environment variable '{env_timeout}'. Using default timeout")
+    timeout_seconds = 8 * 60 * 60
 else:
-    token = None
+    timeout_seconds = timeout * 60
 
 if token == None or token == '':
     print(f"Missing token environment variable '{key}'")
 else:
+    shutdown_timer = threading.Timer(timeout_seconds, on_shutdown)
     client.run(token)
 
+print('Betabot is going to sleep.')
 
