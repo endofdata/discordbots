25074037cf4a66ad9344834e8804f75c2fa61a2f
endofdata <endofdata@hotmail.de>
2019-11-29 23:30:29 +0100

    Another attempt to set a timeout for shutdown of the bot: use an event loop with a shutdown task attached.

--- betabot.py
+++ betabot.py
@@ -5,9 +5,50 @@ import logging
 import asyncio
 
 #------------------------------------------------------------------------------
-# Create a discord client
+# Environment variables used by BetaBot
 #------------------------------------------------------------------------------
-client = discord.Client()
+env_token = 'BETABOT_TOKEN'
+env_timeout = 'BETABOT_TIMEOUT'
+
+# Get the value of an environment variable
+# Parameters
+#   key: name of the variable 
+# Returns
+#   The value, or None if the variable is not set.
+def get_env(key):
+    if key in os.environ:
+        val = os.environ[key]
+    else:
+        val = None
+
+    return val
+
+#------------------------------------------------------------------------------
+# Create a discord client with a shutdown timer
+#------------------------------------------------------------------------------
+
+# Shut down the client connection 
+# This is not an event raised by discord, but from the loop.call_later()
+def on_shutdown():
+    print('Betabot is closing client connection.')
+    asyncio.create_task(client.close())
+
+# Get the shutdown timeout environment variable.
+timeout = get_env(env_timeout)
+
+# If BETABOT_TIMEOUT is not set, use a default of 8 hours.
+if timeout == None or timeout == '':
+    print(f"No timeout environment variable '{env_timeout}'. Using default timeout (8 h).")
+    timeout_seconds = 8 * 60 * 60
+else:
+    timeout_seconds = float(timeout) * 60
+
+# Set up the event loop and attach a delayed call to on_shutdown.
+my_loop = asyncio.get_event_loop()
+my_loop.call_later(timeout_seconds, on_shutdown)
+
+# Finally create the client and let it use the event loop with the timeout.
+client = discord.Client(loop = my_loop)
 
 #------------------------------------------------------------------------------
 # Discord event handlers
@@ -32,15 +73,10 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send(f'Hello, {message.author.name}!')
 
-#------------------------------------------------------------------------------
-# Utility methods
-#------------------------------------------------------------------------------
 
-# Shut down the client connection 
-# This is not raised by discord, but from a timer in this bot
-def on_shutdown():
-    print('Betabot is closing client connection.')
-    asyncio.run(client.close())
+#------------------------------------------------------------------------------
+# Logging example (unused)
+#------------------------------------------------------------------------------
 
 # Example, how to init logging to a file
 # https://discordpy.readthedocs.io/en/latest/logging.html#logging-setup
@@ -51,50 +87,23 @@ def init_logging():
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
 
-# Get the value of an environment variable
-# Parameters
-#   key: name of the variable 
-# Returns
-#   The value, or None if the variable is not set.
-def get_env(key):
-    if key in os.environ:
-        val = os.environ[key]
-    else:
-        val = None
-
-    return val
 
 #------------------------------------------------------------------------------
 # Main
 #------------------------------------------------------------------------------
 print('Betabot is starting up.')
 
-env_token = 'BETABOT_TOKEN'
-env_timeout = 'BETABOT_TIMEOUT'
-
 # Read environment variables for authentication token and shutdown timeout
 token = get_env(env_token)
-timeout = get_env(env_timeout)
-
-# If BETABOT_TIMEOUT is not set, use a default of 8 hours.
-if timeout == None or timeout == '':
-    print(f"No timeout environment variable '{env_timeout}'. Using default timeout (8 h).")
-    timeout_seconds = 8 * 60 * 60
-else:
-    timeout_seconds = float(timeout) * 60
 
 # We cannot run without a valid authentication token.
 if token == None or token == '':
     print(f"Missing token environment variable '{env_token}'.")
 else:
-    # Start a timer to automatically shut down the bot.
-    shutdown_timer = threading.Timer(timeout_seconds, on_shutdown)
-    shutdown_timer.start()
-
     # Run the discord client to receive discord events.
     client.run(token)
 
+    # Since client.run() is blocking until the end of the event loop, we get
+    # here only, after the shutdown timer has fired or an error occured.
     print('Betabot is going to sleep.')
 
-    # Clear the time in case of client.run() returned w/o timeout.
-    shutdown_timer.cancel()
